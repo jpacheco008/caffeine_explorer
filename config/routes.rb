0eab7a5d9febb227d78affe0ac7bc873188925f3
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-01-30 08:36:29 -0500

    controller for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :coffees
   resources :comments
-  resources :favorites_coffee
+  resources :favorites
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users
