a27eec4bf73c9d9aa4e4ad9687ba61d4f6ca9e8a
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-01 14:25:28 -0500

    styled coffee component

--- client/src/components/Coffee.jsx
+++ client/src/components/Coffee.jsx
@@ -1,12 +1,13 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/Coffee.css"
 
 export default function Coffee(props) {
   const { id, blend_name, imgURL } = props;
   return (
-    <Link to={`/coffees/${id}`}>
-      <div>
-      <h3>{blend_name}</h3>
-      <img src={imgURL} alt="Can't find the beans"/>
+    <Link className='coffee' to={`/coffees/${id}`}>
+      <div className='details-container'>
+      <h3 className='blend-name'>{blend_name}</h3>
+      <img className='coffee-img' src={imgURL} alt="Can't find the beans"/>
       </div>
     </Link>
     )
