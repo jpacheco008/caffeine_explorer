84a87fe70916478070ac386c3f5393f4c196f25c
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-02 14:52:18 -0500

    added delete buttons on profile

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -1,13 +1,12 @@
-import { getFavorites } from '../services/favorites'
-import { useState, useEffect } from 'react';
-import Coffee from '../components/Coffee'
-import '../styles/Profile.css'
-import { Link } from 'react-router-dom';
+import { getFavorites } from "../services/favorites";
+import { useState, useEffect } from "react";
+import Coffee from "../components/Coffee";
+import "../styles/Profile.css";
+import { Link } from "react-router-dom";
 
 export default function Profile(props) {
-  
   const [profileData, setProfileData] = useState([]);
-  
+
   useEffect(() => {
     const fetchFavorites = async () => {
       const id = props.currentUser && props.currentUser.id;
@@ -17,24 +16,34 @@ export default function Profile(props) {
     fetchFavorites();
   }, [props.currentUser]);
 
-  return (  
-    <div className='profile-list-container'>
-      <div className='profile-welcome-list-container'>
-    <h3 className='profile-welcome'>{props.currentUser && props.currentUser.username}'s favorites list:</h3>
-      <div className='profile-list'>
-      {profileData.favorites && profileData.favorites.map((list, index) => {
-        return <Coffee
-        id={list.coffee.id}
-        blend_name={list.coffee.blend_name}
-        imgURL={list.coffee.picture}
-        key={index}
-        />
-      })}
+  return (
+    <div className="profile-list-container">
+      <div className="profile-welcome-list-container">
+        <h3 className="profile-welcome">
+          {props.currentUser && props.currentUser.username}'s favorites list:
+        </h3>
+        <div className="profile-list">
+          {profileData.favorites &&
+            profileData.favorites.map((list, index) => {
+              return (
+                <div className='profile-coffee-container'>
+                <Coffee
+                  id={list.coffee.id}
+                  blend_name={list.coffee.blend_name}
+                  imgURL={list.coffee.picture}
+                  key={index}
+                  />
+                <button className='profile-coffee-button'>Remove</button>
+                  </div>
+              );
+            })}
+        </div>
       </div>
-      </div>
-      <div className='profile-nav'>
-      <Link to='/coffees' className='profile-links'>To coffees</Link>
+      <div className="profile-nav">
+        <Link to="/coffees" className="profile-links">
+          To coffees
+        </Link>
       </div>
     </div>
-  )
+  );
 }
