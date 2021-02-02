22a2081a8f678e837913c2af7d62a037d631a43c
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-02 15:02:47 -0500

    added favorites services

--- client/src/services/favorites.js
+++ client/src/services/favorites.js
@@ -4,3 +4,11 @@ export const getFavorites = async (id) => {
   const resp = await api.get(`/users/${id}`)
   return resp.data
 }
+export const postFavorites = async (favoriteData) => {
+  const resp = await api.post('/favorites', { favotire: favoriteData });
+  return resp.data
+}
+export const deleteFavorites = async (id) => {
+  const resp = await api.delete(`/favorites/${id}`)
+  return resp
+}
