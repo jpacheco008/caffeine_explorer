b527ebc4369a34e185790053805c8a82db0d6507
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-02 11:13:47 -0500

    passable landing

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -1,12 +1,15 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import '../styles/Landing.css'
 
 export default function Landing(props) {
   const { currentUser } = props
   
   return (
-    <div currentUser={currentUser}>
-      <h1>A taste adventure for the caffeine afficionados</h1>
-      <Link to='/coffees'>Click here to begin your journey</Link>
+    <div currentUser={currentUser} className='landing-container'>
+      <div className='text-container'>
+      <h1 className='landing-title'>A taste adventure for the caffeine afficionados</h1>
+      <Link to='/coffees' className='landing-link'>Click here to begin your journey</Link>
+      </div>
     </div>
   )
 }
